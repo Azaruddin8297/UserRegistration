02274d2ddd6e6168a75444061e8fd03c28fb754b
Azaruddin8297 <ajarpopo.com>
2023-03-30 23:28:10 +0530

    Name

---
+++ UserReg/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UserReg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The FirstName");
+            string Name = Console.ReadLine();
+           bool val = validation.Check(Name);
+            //Console.WriteLine(val);
+            if (val)
+            {
+                Console.WriteLine("FirstName is {0}", Name);
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Name");
+            }
+        }
+    }
+}
