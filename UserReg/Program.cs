ca9589fede86f4d817f4020ba3986f31a3ea11b2
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-31 19:45:02 +0530

    MobileNumber

--- UserReg/Program.cs
+++ UserReg/Program.cs
@@ -4,34 +4,39 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Enter The FirstName");
-            //string Name = Console.ReadLine();
-            //bool val = validation.Check(Name);
-            //if (val)
-            //{
-            //    Console.WriteLine("FirstName is {0}", Name);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Enter Valid Name");
-            //}
-            //Console.WriteLine("Enter The LastName");
-            //string LName = Console.ReadLine();
-            //bool val1 = validation.Check(LName);
-            //if (val1)
-            //{
-            //    Console.WriteLine("LastName is {0}", LName);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Enter Valid Name");
-            //}
-            //Console.WriteLine("FullName is {0} {1}",Name,LName);
+            Console.WriteLine("Enter The FirstName");
+            string Name = Console.ReadLine();
+            bool val = validation.Check(Name);
+            if (val)
+            {
+                Console.WriteLine("FirstName is {0}", Name);
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Name");
+            }
+            Console.WriteLine("Enter The LastName");
+            string LName = Console.ReadLine();
+            bool val1 = validation.Check(LName);
+            if (val1)
+            {
+                Console.WriteLine("LastName is {0}", LName);
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Name");
+            }
+            Console.WriteLine("FullName is {0} {1}", Name, LName);
             Console.WriteLine("***************************************");
             Console.WriteLine("Enter Valid Email ID");
             validation v = new validation();
             string EN = Console.ReadLine();
             v.ValidateEmail(EN);
+            Console.WriteLine("***************************************");
+            Console.WriteLine("Enter Valid Phone Number");
+            validation v1 = new validation();
+            string PH = Console.ReadLine();
+            v1.ValidatePhone(PH);
         }
     }
 }
--- UserReg/validation.cs
+++ UserReg/validation.cs
@@ -12,6 +12,7 @@ namespace UserReg
         public static string FirstName = "^[A-Z]{1}[a-z]{3,}$";
         public static string LastName = "^[A-Z]{1}[a-z]{3,}$";
         public static string Email = "^[A-Za-z0-9]+[@][a-z]+[.][a-z]{2,3}$";
+        public static string Phone_Number = "^[9][1][ ][6-9]{1}[0-9]{9}$";
         public static bool Check(string name)
         {
             return Regex.IsMatch(name, FirstName);
@@ -24,6 +25,19 @@ namespace UserReg
         {
             Regex reg = new Regex(Email);
             bool valid = reg.IsMatch(mail);
+            if (valid)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void ValidatePhone(string phone)
+        {
+            Regex reg = new Regex(Phone_Number);
+            bool valid = reg.IsMatch(phone);
             if (valid)
             {
                 Console.WriteLine("Valid");
