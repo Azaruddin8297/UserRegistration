1551d7f17d06ad1095439a6ced6c2af2698b1c82
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-31 19:34:45 +0530

    EmailValidation

--- UserReg/Program.cs
+++ UserReg/Program.cs
@@ -4,28 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter The FirstName");
-            string Name = Console.ReadLine();
-            bool val = validation.Check(Name);
-            if (val)
-            {
-                Console.WriteLine("FirstName is {0}", Name);
-            }
-            else
-            {
-                Console.WriteLine("Enter Valid Name");
-            }
-            Console.WriteLine("Enter The LastName");
-            string LName = Console.ReadLine();
-            bool val1 = validation.Check(LName);
-            if (val1)
-            {
-                Console.WriteLine("FirstName is {0}", LName);
-            }
-            else
-            {
-                Console.WriteLine("Enter Valid Name");
-            }
+            //Console.WriteLine("Enter The FirstName");
+            //string Name = Console.ReadLine();
+            //bool val = validation.Check(Name);
+            //if (val)
+            //{
+            //    Console.WriteLine("FirstName is {0}", Name);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter Valid Name");
+            //}
+            //Console.WriteLine("Enter The LastName");
+            //string LName = Console.ReadLine();
+            //bool val1 = validation.Check(LName);
+            //if (val1)
+            //{
+            //    Console.WriteLine("LastName is {0}", LName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter Valid Name");
+            //}
+            //Console.WriteLine("FullName is {0} {1}",Name,LName);
+            Console.WriteLine("***************************************");
+            Console.WriteLine("Enter Valid Email ID");
+            validation v = new validation();
+            string EN = Console.ReadLine();
+            v.ValidateEmail(EN);
         }
     }
 }
--- UserReg/validation.cs
+++ UserReg/validation.cs
@@ -9,8 +9,9 @@ namespace UserReg
 {
     public class validation
     {
-        public static string FirstName = "^[A-Z]{1}[a-z]{3}$";
-        public static string LastName = "^[A-Z]{1}[a-z]{3}$";
+        public static string FirstName = "^[A-Z]{1}[a-z]{3,}$";
+        public static string LastName = "^[A-Z]{1}[a-z]{3,}$";
+        public static string Email = "^[A-Za-z0-9]+[@][a-z]+[.][a-z]{2,3}$";
         public static bool Check(string name)
         {
             return Regex.IsMatch(name, FirstName);
@@ -18,6 +19,19 @@ namespace UserReg
         public static bool Check2(string name1)
         {
             return Regex.IsMatch(name1, LastName);
+        }
+        public void ValidateEmail(string mail)
+        {
+            Regex reg = new Regex(Email);
+            bool valid = reg.IsMatch(mail);
+            if (valid)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
     }
 }
