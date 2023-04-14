7566e084a919b6304d2a9f1f5640926138b99463
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-04-14 18:48:47 +0530

    Testing

--- UserReg/Program.cs
+++ UserReg/Program.cs
@@ -1,25 +1,84 @@
-﻿namespace MoodAnalyzer
+﻿namespace UserReg
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            validation validation = new validation();
+           bool a = validation.ValidatePassword("Ajar1234");
+            bool b = validation.ValidatePassword1("Ajar1234");
+            bool c = validation.ValidatePassword2("Ajar1234");
+            bool d = validation.ValidatePassword3("Ajar1234");
+            Console.WriteLine(a+ " " + b + " " + c + " " + d);
+            //Console.WriteLine("Enter The FirstName");
+            //string name = Console.ReadLine();
+            //bool val = validation.Check(name);
+            //if (val)
+            //{
+            //    Console.WriteLine("FirstName is {0}", name);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter Valid Name");
+            //}
+            //Console.WriteLine("Enter The LastName");
+            //string lName = Console.ReadLine();
+            //bool val1 = validation.Check(lName);
+            //if (val1)
+            //{
+            //    Console.WriteLine("LastName is {0}", lName);
+            //}
+            //else
+            //{
 
-           
-            MoodTest mt1 = new MoodTest("I am Any Thing");
-            Console.WriteLine(mt1.Analyzer());
+            //    Console.WriteLine("Enter Valid Name");
+            //}
+            //Console.WriteLine("FullName is {0} {1}", name, lName);
 
-            Console.WriteLine("***********************");
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine("Enter Valid Email ID");
+            //validation v = new validation();
+            //string email = Console.ReadLine();
+            //v.ValidateEmail(email);
 
-            MoodTest mt3 = new MoodTest(null);
-            Console.WriteLine(mt3.Analyzer());
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine("Enter Valid Phone Number");
+            //validation v1 = new validation();
+            //string phone = Console.ReadLine();
+            //v1.ValidatePhone(phone);
 
-            Console.WriteLine("***********************");
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine("Enter Valid Password");
+            //validation p1 = new validation();
+            //string pass = Console.ReadLine();
+            //p1.ValidatePassword(pass);
 
-            MoodTest mt = new MoodTest(string.Empty);
-            Console.WriteLine(mt.Analyzer());
-            
-            
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine("Enter Valid Password with atleast 1 upperCase ");
+            //validation p2 = new validation();
+            //string pass1 = Console.ReadLine();
+            //p2.ValidatePassword1(pass1);
+
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine("Enter Valid Password with atleast 1 number");
+            //validation p3 = new validation();
+            //string pass2 = Console.ReadLine();
+            //p3.ValidatePassword2(pass2);
+
+            //Console.WriteLine("***************************************");
+            //Console.WriteLine("Enter Valid Password with 1 upperCase and 1 number");
+            //validation p4 = new validation();
+            ////string pass3 = Console.ReadLine();
+            ////p4.ValidatePassword2(pass3);
+
+            //Console.WriteLine("***************************************");
+            //validation v10 = new validation();
+            //string[] em = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.net", "abc-100@abc.net", "abc.100@abc.com.au", "abc+100@gmail.com", };
+            //foreach (string s in em)
+            //{
+            //    v10.ValidateEmail(s);
+
+            //}
         }
     }
 }
--- UserReg/validation.cs
+++ UserReg/validation.cs
@@ -9,11 +9,117 @@ namespace UserReg
 {
     public class validation
     {
-        public static string FirstName = "^[A-Z]{1}[a-z]{3}$";
+        public static string FirstName = "^[A-Z]{1}[a-z]{3,}$";
+        public static string LastName = "^[A-Z]{1}[a-z]{3,}$";
+        public static string Email = "^[0-9A-Za-z]+[.+-_]{0,1}[0-9A-Za-z]+[@][]A-Za-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
+        public static string Phone_Number = "^[9][1][ ][6-9]{1}[0-9]{9}$";
+        public static string Password = "^[a-zA-Z0-9]{8}$";
+        public static string Password1 = "^(?=.*[A-Z])[a-zA-Z0-9]{8}$";
+        public static string Password2 = "^(?=.*[0-9])[a-zA-Z0-9]{8}$";
+        public static string Password3 = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{8}$";
+
 
         public static bool Check(string name)
         {
-            return Regex.IsMatch(name, FirstName);
+           bool result = Regex.IsMatch(name, FirstName);
+            if(result==true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool Check2(string name1)
+        {
+            bool result = Regex.IsMatch(name1, LastName);
+            if (result == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidateEmail(string mail)
+        {
+            Regex reg = new Regex(Email);
+            bool valid = reg.IsMatch(mail);
+            if (valid == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidatePhone(string phone)
+        {
+            Regex reg = new Regex(Phone_Number);
+            bool valid = reg.IsMatch(phone);
+            if (valid == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidatePassword(string password)
+        {
+            Regex reg = new Regex(Password);
+            bool valid = reg.IsMatch(password);
+            if (valid == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidatePassword1(string password1)
+        {
+            Regex reg = new Regex(Password1);
+            bool valid = reg.IsMatch(password1);
+            if (valid == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidatePassword2(string password2)
+        {
+            Regex reg = new Regex(Password2);
+            bool valid = reg.IsMatch(password2);
+            if (valid == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidatePassword3(string password3)
+        {
+            Regex reg = new Regex(Password3);
+            bool valid = reg.IsMatch(password3);
+          if (valid == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
